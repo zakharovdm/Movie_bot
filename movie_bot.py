402a269d4510acfee62b1dc564a385a3af3b4847
Dmitry Zakharov <dzaharov794@gmail.com>
2018-12-19 00:41:45 +0300

    Добавил CommandHandler, добавил кнопки, написал функцию приветсвия по нажатию старт

--- movie_bot.py
+++ movie_bot.py
@@ -1,8 +1,25 @@
-from telegram.ext import Updater 
+import greeting
 import settings
+
+from telegram.ext import Updater, CommandHandler 
+from telegram import ReplyKeyboardMarkup
+
+
+
+
+
+
+def greet_user(bot,update):
+	ave_text = 'Привет {}! {} '.format(update.message.chat.first_name, greeting.greet_text) 
+	menu_keyboard = ReplyKeyboardMarkup([['Поиск фильма', 'Поиск актера']])
+	update.message.reply_text(ave_text, reply_markup = menu_keyboard)
+
 
 def main():
 	moviebot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
+
+	dp = moviebot.dispatcher
+	dp.add_handler(CommandHandler('start', greet_user))
 
 	
 
